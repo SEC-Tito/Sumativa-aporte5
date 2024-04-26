fd7444ccb3010d9b879a12b595f87f69a30a68be
SEC-Tito <mateoenclada52@gmail.com>
2024-04-26 09:40:53 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
 
+  async ngOnInit() {
+    await LocalNotifications.requestPermissions();//solicitar permisos de la app
+    await LocalNotifications.schedule({//Elaboracion del objeto notificacion
+      notifications: [
+        {
+          title: "😎",
+          body: "si",
+          id: 1
+        }
+      ]
+    });
+  }
 }
